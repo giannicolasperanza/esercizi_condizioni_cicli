9cacdb439ec70985e39456e5bab08aeb28d58c3c
Giannicola Speranza <speranzagiannicola@gmail.com>
2025-03-12 18:50:09 +0100

    secondo esercizio

--- main.js
+++ main.js
@@ -29,30 +29,88 @@
 
 // ---------------------------------------------------------------
 
-let v = Number(prompt("Inserisci il voto"));
+// let v = Number(prompt("Inserisci il voto"));
+
+// switch(true){
+
+//     case v < 18:
+//         console.log(`insufficiente`);
+//             break;
+    
+//     case v >= 18 && v < 21:
+//         console.log(`buono`);
+//             break;
+    
+//     case v >= 24 && v < 27:
+//         console.log(`distinto`);
+//             break;
+    
+//     case v >= 27 && v <= 29:
+//         console.log(`ottimo`);
+//             break;
+    
+//     case  v == 30:
+//         console.log(`eccellente`);
+//             break;
+
+//     default: console.log(`il voto inserito non è valido`);
+            
+//         }
+
+// --------------------------fine primo esercizio -------------------------------------
+
+// --------------------------inizio secondo esercizio ---------------------------------
+
+// let temperatura = Number(prompt(`Inserisci la temperatura`));
+
+// if(temperatura < 20 && temperatura >= 0){
+//     console.log("non ci sono più le mezze stagioni");
+    
+// }else if(temperatura >= 30 && temperatura<= 50 ){
+//     console.log("lu mare, lu sole, lu ientu");
+    
+// }else if(temperatura < 30 && temperatura >= 20){
+//     console.log("mi dia una peroni sudata");
+    
+// }else if(temperatura < 0 && temperatura >= -10){
+//     console.log("non è tanto il freddo quanto l'umidità");
+    
+// }else if(temperatura < -10 && temperatura >= -30){
+//     console.log("copriti...ancora ti raffreddi");
+    
+// }else{
+//     console.log("Ma che temperatura hai messo?");
+    
+// }
+
+
+
+let temperatura = Number(prompt(`Inserisci la temperatura`));
 
 switch(true){
 
-    case v < 18:
-        console.log(`insufficiente`);
+        case temperatura < 20 && temperatura >= 0:
+           console.log("non ci sono più le mezze stagioni");
             break;
-    
-    case v >= 18 && v < 21:
-        console.log(`buono`);
-            break;
-    
-    case v >= 24 && v < 27:
-        console.log(`distinto`);
-            break;
-    
-    case v >= 27 && v <= 29:
-        console.log(`ottimo`);
-            break;
-    
-    case  v == 30:
-        console.log(`eccellente`);
-            break;
+        
+        case temperatura >= 30 && temperatura<= 50:
+            console.log("lu mare, lu sole, lu ientu");
+                break;
 
-    default: console.log(`il voto inserito non è valido`);
-            
-        }
+        case temperatura < 30 && temperatura >= 20:
+            console.log("mi dia una peroni sudata");
+                break;
+
+        case temperatura < 0 && temperatura >= -10:
+            console.log("non è tanto il freddo quanto l'umidità");
+                break;
+
+        case temperatura < -10 && temperatura >= -30:
+            console.log("copriti...ancora ti raffreddi");
+                break;
+
+        default:console.log("Ma che temperatura hai messo?");            
+                
+}
+
+// --------------------------fine secondo esercizio ---------------------------------
